c64aac4549827d46039d8648dd06557623055ef1
Anthony Garcia <agarcia@ncsasports.org>
2016-09-28 10:21:31 -0500

    Quick dirty initial component to test build pipeline.

---
+++ src/Hello.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Hello extends Component {
+  render() {
+    return (
+      <p>{this.props.greeting} World!</p>
+    );
+  }
+}
+
+export default Hello;
