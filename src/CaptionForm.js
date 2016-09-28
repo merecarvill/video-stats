a185113eb0bdf8baebf7d26ce440e324134f5875
Spencer Carvill <scarvill@8thlight.com>
2016-09-28 14:21:00 -0500

    Store captions to browser session

--- src/CaptionForm.js
+++ src/CaptionForm.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import Storage from "./storage.js";
+
 injectTapEventPlugin();
 
 class CaptionForm extends Component {
@@ -12,21 +14,25 @@ class CaptionForm extends Component {
       caption: "",
       startTime: "",
       duration: 5
-    }
+    };
+    this.storage = new Storage();
+    this.storage.clear();
 
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
+    this.saveCaption = this.saveCaption.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
   }
 
   componentWillReceiveProps(nextProps) {
     this.setState({startTime: nextProps.startTime});
   }
 
-  handleSubmit() {
-    console.log("submit");
+  saveCaption() {
+    const currentCaptions = this.storage.get("captions") || [];
+    this.storage.set("captions", currentCaptions.concat(this.state));
+    this.setState({ caption: "" });
   }
 
-  handleChange(e) {
+  handleInputChange(e) {
     const change = {};
     const fieldName = e.target.name;
     const fieldValue = e.target.value;
@@ -43,26 +49,26 @@ class CaptionForm extends Component {
             placeholder="Enter caption"
             name="caption"
             value={this.state.caption}
-            onChange={this.handleChange}
+            onChange={this.handleInputChange}
           />
           <br/>
           <TextField
             placeholder="Start time"
             name="startTime"
             value={this.state.startTime}
-            onChange={this.handleChange}
+            onChange={this.handleInputChange}
           />
           <br/>
           <TextField
             placeholder="Duration"
             name="duration"
             value={this.state.duration}
-            onChange={this.handleChange}
+            onChange={this.handleInputChange}
           />
           <br/>
           <RaisedButton
             label="Save"
-            onClick={this.handleSubmit}  primary={true}
+            onClick={this.saveCaption}  primary={true}
           />
         </form>
       );
---
+++ src/storage.js
@@ -0,0 +1,15 @@
+class Storage {
+  get(key) {
+    return JSON.parse(localStorage.getItem(key));
+  }
+
+  set(key, value) {
+    localStorage.setItem(key, JSON.stringify(value));
+  }
+
+  clear() {
+    localStorage.clear();
+  }
+}
+
+export default Storage;
